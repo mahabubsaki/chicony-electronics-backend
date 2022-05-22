1c6e71a1d5dadeae3986795d6df8e75c880083bb
mahabubsaki <96987600+mahabubsaki@users.noreply.github.com>
2022-05-22 20:04:01 +0600

    connected to datatbase

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_OWNER}:${process.env.DB_PASSWORD}@cluster0.wcxgg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+const run = async () => {
+    try {
+        await client.connect();
+        console.log('connect success')
+    }
+    finally { }
+}
+run().catch(console.dir)
